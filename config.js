c152922ad0f2f64c160d136f99a3d7df01f0c5c6
CUSTOMWEBDEVS <116915610+CUSTOMWEBDEVS@users.noreply.github.com>
2025-12-13 23:29:05 -0600

    Replace GAS_WEBAPP_URL with API_URL

--- config.js
+++ config.js
@@ -1,2 +1,1 @@
-// Set this once (your deployed Apps Script Web App URL)
-window.GAS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbzpMw5H3bStsQLB-MeaYaiPMRBJZPBQ56yF0CzHjafCL6LnMhjVKf8Z2bvy-itZtmM-/exec";
+window.API_URL = "https://script.google.com/macros/s/AKfycbzpMw5H3bStsQLB-MeaYaiPMRBJZPBQ56yF0CzHjafCL6LnMhjVKf8Z2bvy-itZtmM-/exec";
