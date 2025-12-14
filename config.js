0842a27675972c62e073b04003c440895970de02
CUSTOMWEBDEVS <116915610+CUSTOMWEBDEVS@users.noreply.github.com>
2025-12-13 23:22:34 -0600

    Update GAS_WEBAPP_URL to production URL

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Set this once (your deployed Apps Script Web App URL)
-window.GAS_WEBAPP_URL = "PASTE_YOUR_GAS_EXEC_URL_HERE";
+window.GAS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbzpMw5H3bStsQLB-MeaYaiPMRBJZPBQ56yF0CzHjafCL6LnMhjVKf8Z2bvy-itZtmM-/exec";
